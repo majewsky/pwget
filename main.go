689a9d83d91203759a3f6718065594addc405367
Stefan Majewsky <majewsky@gmx.net>
2016-03-19 23:36:13 +0100

    implement

--- main.go
+++ main.go
@@ -19,18 +19,185 @@
 
 package main
 
-import "./localdeps/golang.org/x/crypto/scrypt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+
+	"./localdeps/golang.org/x/crypto/scrypt"
+)
+
+/*
+#cgo LDFLAGS: -lcrypto
+#cgo CFLAGS: -Wno-deprecated-declarations
+#include <stdio.h>
+#include <openssl/ui.h>
+*/
+import "C"
+
+func main() {
+	//check arguments
+	domain, doRevoke := ParseArguments()
+
+	//load revocation list
+	isSHA256OfRevokedPassword, err := LoadRevocationList()
+	FailOnError("load revocation list", err)
+
+	//get master password
+	masterPassword, err := GetMasterPassword()
+	FailOnError("password query", err)
+
+	//now we're all set, derive the password
+	var passwordStr, hashStr string
+	for iteration := 0; true; iteration++ {
+		//get the password for this iteration
+		salt := []byte(strconv.Itoa(iteration) + ":" + domain)
+		password := Scrypt(masterPassword, salt)
+		passwordStr = hex.EncodeToString(password)
+
+		//this is the correct password, unless it has been revoked
+		hash := sha256.Sum256([]byte(passwordStr))
+		hashStr = hex.EncodeToString(hash[:])
+		if isSHA256OfRevokedPassword[hashStr] {
+			os.Stderr.Write([]byte("\x1B[37m" + passwordStr + " is revoked\x1B[0m\n"))
+			continue
+		}
+		break
+	}
+
+	//now, we either print or revoke the thing
+	if doRevoke {
+		os.Stderr.Write([]byte("Revoking " + passwordStr + "\n"))
+		err := AppendToRevocationList(hashStr)
+		FailOnError("update revocation list", err)
+	} else {
+		os.Stdout.Write([]byte(passwordStr))
+		os.Stdout.Sync()
+		//write the newline on stderr only, so that it is not copied when
+		//piping stdout to xsel or xclip
+		os.Stderr.Write([]byte("\n"))
+	}
+}
+
+//FailOnError prints the given error and does not return (unless it is nil).
+func FailOnError(operation string, err error) {
+	if err != nil {
+		os.Stderr.Write([]byte(operation + " failed: " + err.Error() + "\n"))
+		os.Exit(1)
+	}
+}
+
+//ParseArguments parses the os.Args. Will not return if they are malformed.
+func ParseArguments() (domain string, revoke bool) {
+	usage := []byte("Usage: " + os.Args[0] + " [-r|--revoke] <domain>\n")
+
+	//need at least one argument
+	if len(os.Args) < 2 {
+		os.Stderr.Write(usage)
+		os.Exit(1)
+	}
+
+	//read arguments
+	var argRevoke = false
+	var argDomain string
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "-h", "--help":
+			os.Stderr.Write(usage)
+			os.Exit(0)
+		case "-r", "--revoke":
+			argRevoke = true
+		default:
+			if argDomain != "" {
+				os.Stderr.Write([]byte("error: multiple domains\n"))
+				os.Exit(1)
+			}
+			argDomain = arg
+		}
+	}
+
+	//need domain to continue
+	if argDomain == "" {
+		os.Stderr.Write(usage)
+		os.Exit(1)
+	}
+	return argDomain, argRevoke
+}
+
+//GetMasterPassword queries the user for the master password. Will not return
+//if an error occurs. This implementation is copied from [1], but specialized
+//to the particular usecase.
+//
+//[1] https://github.com/bslatkin/getpass/blob/ef7bd7b01e3d5412b3a5f25fe5348ccf18f19751/getpass.go
+func GetMasterPassword() ([]byte, error) {
+	bufferSize := 256
+	buffer := C.malloc(C.size_t(bufferSize))
+	defer C.free(buffer)
+	bufferPtr := (*C.char)(buffer)
+
+	returnCode, err := C.UI_UTIL_read_pw_string(
+		bufferPtr, C.int(bufferSize),
+		C.CString("Master password: "),
+		C.int(0), //don't require confirmation
+	)
+
+	if returnCode != 0 {
+		return nil, err
+	}
+	return []byte(C.GoString(bufferPtr)), nil
+}
+
+func revocationListPath() string {
+	return os.Getenv("HOME") + "/.pwget-revocation"
+}
+
+//LoadRevocationList reads the revocation list, which contains the SHA256
+//hashes of all passwords which have been revoked.
+func LoadRevocationList() (map[string]bool, error) {
+	//try to read file
+	contents, err := ioutil.ReadFile(revocationListPath())
+	if err != nil {
+		if os.IsNotExist(err) {
+			return map[string]bool{}, nil
+		}
+		return nil, err
+	}
+
+	//each hash is on one line
+	hashes := strings.Split(string(contents), "\n")
+	result := make(map[string]bool)
+	for _, hash := range hashes {
+		if hash != "" {
+			result[hash] = true
+		}
+	}
+	return result, nil
+}
+
+//AppendToRevocationList adds a new password hash to the revocation list.
+func AppendToRevocationList(hash string) error {
+	file, err := os.OpenFile(revocationListPath(), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write([]byte(hash + "\n"))
+	if err != nil {
+		return err
+	}
+
+	return file.Close()
+}
 
 //Scrypt wraps scrypt.Key() and defines its parameters so that the KDF always
 //produces the same results within the scope of this program.
 func Scrypt(password, salt []byte) []byte {
-	result, err := scrypt.Key(password, salt, 1<<20, 8, 16, 32)
+	result, err := scrypt.Key(password, salt, 1<<16, 8, 16, 32)
 	if err != nil {
 		panic(err.Error())
 	}
 	return result
-}
-
-func main() {
-	panic("TODO")
 }
